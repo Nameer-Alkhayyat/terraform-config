5c311bc80d45926e32987f5cccbba27b1990a1be
Nameer <nameer.alkhayyat@code.berlin>
2022-10-16 16:17:58 +0200

    add api gatway instance

---
+++ aws-backend/api-gateway.tf
@@ -0,0 +1,43 @@
+
+resource "aws_api_gateway_rest_api" "api" {
+  name = var.stage == "stage" ? "${var.projectName}-stage" :  "${var.projectName}"
+  description = "api-gateway for ${var.projectName} ${var.stage}"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "{proxy+}"
+}
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "ANY"
+  authorization = "NONE"
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.ic-lambda.invoke_arn
+ 
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [aws_api_gateway_integration.integration]
+
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "development"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
