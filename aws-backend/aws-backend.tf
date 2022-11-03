65785940a1c14342855741fbe310b66097529bab
Nameer <nameer.alkhayyat@code.berlin>
2022-11-03 11:30:30 +0100

    changed data team

--- aws-backend/aws-backend.tf
+++ aws-backend/aws-backend.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_function" "ic-lambda" {
-  filename      = "data/HelloWorld.zip"
+  filename      = "HelloWorld.zip"
   function_name = var.stage == "stage" ? "${var.projectName}-stage" :  "${var.projectName}"
 
   handler = "index.handler"
